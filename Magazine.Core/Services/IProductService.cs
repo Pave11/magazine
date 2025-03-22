94a0d2534f23aec6a3ef3222f76cd70a9a1f46f6
Pavel <perkin.pavel04@gmail.com>
2025-03-22 09:13:59 +0300

    добавили класс ProductService реализующий интерфейс IProductService.

--- Magazine.Core/Services/IProductService.cs
+++ Magazine.Core/Services/IProductService.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Magazine.Core.Models;
 
 namespace Magazine.Core.Services
 {
-    internal interface IProductService
+    public interface IProductService
     {
+        // Метод для добавления нового элемента
+        Product Add(Product product);
+
+        // Метод для удаления элемента
+        Product Remove(Guid id);
+
+        // Метод для изменения элемента
+        Product Edit(Product product);
+
+        // Метод для поиска элемента по параметрам
+        Product Search(Guid id);
     }
 }
---
+++ Magazine.WebApi/ProductService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Magazine.Core.Models;
+using Magazine.Core.Services;
+
+namespace Magazine.WebApi.Services
+{
+    public class ProductService : IProductService
+    {
+        // Здесь вы можете использовать коллекцию для хранения продуктов
+        // В реальном приложении вы бы использовали контекст базы данных
+        private readonly List<Product> _products = new List<Product>();
+
+        public Product Add(Product product)
+        {
+            try
+            {
+                // Добавление нового продукта
+                _products.Add(product);
+                return product; // Возвращаем созданный продукт
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений должна происходить в вызывающем методе
+                throw new Exception("Ошибка при добавлении продукта", ex);
+            }
+        }
+
+        public Product Remove(Guid id)
+        {
+            try
+            {
+                // Поиск продукта по идентификатору
+                var product = _products.Find(p => p.Id == id);
+                if (product == null)
+                {
+                    return null; // Если продукт не найден, возвращаем null
+                }
+
+                // Удаление продукта
+                _products.Remove(product);
+                return product; // Возвращаем удалённый продукт
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений должна происходить в вызывающем методе
+                throw new Exception("Ошибка при удалении продукта", ex);
+            }
+        }
+
+        public Product Edit(Product product)
+        {
+            try
+            {
+                // Поиск продукта по идентификатору
+                var existingProduct = _products.Find(p => p.Id == product.Id);
+                if (existingProduct == null)
+                {
+                    return null; // Если продукт не найден, возвращаем null
+                }
+
+                // Обновление данных продукта
+                existingProduct.Name = product.Name;
+                existingProduct.Definition = product.Definition;
+                existingProduct.Price = product.Price;
+                existingProduct.Image = product.Image;
+
+                return existingProduct; // Возвращаем изменённый продукт
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений должна происходить в вызывающем методе
+                throw new Exception("Ошибка при редактировании продукта", ex);
+            }
+        }
+
+        public Product Search(Guid id)
+        {
+            try
+            {
+                // Поиск продукта по идентификатору
+                return _products.Find(p => p.Id == id); // Возвращаем найденный продукт или null
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений должна происходить в вызывающем методе
+                throw new Exception("Ошибка при поиске продукта", ex);
+            }
+        }
+    }
+}
